c2787080cd7f91b2512622a751b72a4048c4bcbc
Aurélien Matouillot <a.matouillot@gmail.com>
2015-05-21 07:52:31 +0200

    Center the message

--- app/scripts/directives/message.js
+++ app/scripts/directives/message.js
@@ -11,8 +11,16 @@ angular.module('waxeApp')
         return {
             template: '<div class="alert-message alert alert-{{MessageService.type}}" ng-class="MessageService.animation" ng-if="MessageService.message">{{MessageService.message}}<button type="button" class="close" ng-click="MessageService.close()">x</button></div>',
             restrict: 'E',
-            link: function postLink(scope) {
+            link: function postLink(scope, element) {
                 scope.MessageService = MessageService;
+                scope.$watch(function(){
+                    if (scope.MessageService.message) {
+                        var $elt = element.children().eq(0);
+                        var left = ($('body').width() - $elt.outerWidth()) / 2;
+                        $elt.css({left: left + 'px'});
+                    }
+
+                });
             }
         };
     }]);
