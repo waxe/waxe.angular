37b3e0c74317894e3f43e0476e32079fa00bcab6
Aurélien Matouillot <a.matouillot@gmail.com>
2015-05-21 07:40:04 +0200

    Automatically hide the success message

--- app/scripts/services/message.js
+++ app/scripts/services/message.js
@@ -8,16 +8,28 @@
  * Service in the waxeApp.
  */
 angular.module('waxeApp')
-    .service('MessageService', function () {
+    .service('MessageService', ['$timeout', function ($timeout) {
         this.message = null;
         this.type = null;
+        this.timer = null;
 
         this.set = function(type, message) {
+            if (this.timer) {
+                // Stop the timer
+                $timeout.cancel(this.timer);
+            }
             this.type = type;
             this.message = message;
+
+            if (this.type === 'success') {
+                var that = this;
+                this.timer = $timeout(function () {
+                    that.close();
+                }, 500);
+            }
         };
 
         this.close = function() {
             this.message = null;
         };
-    });
+    }]);
