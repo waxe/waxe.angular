3cc3d081cdc23fba52f889852ea85275f1ba2af8
Aurélien Matouillot <aurelien@gymglish.com>
2015-08-06 09:13:50 +0200

    Be able to pass a path to edit when accessing to the home

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,12 +10,23 @@
 angular.module('waxeApp')
     .controller('MainCtrl', ['$scope', '$location', 'UrlFactory', 'UserProfile', 'Session', function ($scope, $location, UrlFactory, UserProfile, Session) {
 
+        var path = $location.search().path;
+
         if (UserProfile.has_file === true) {
-            $location.url(UrlFactory.userUrl());
+            var url;
+            if (angular.isDefined(path)) {
+                // TODO: remove this hardcoded path when we will be able to edit more filetype
+                url = UrlFactory.userUrl('xml/edit', {path: path});
+            }
+            else {
+                url = UrlFactory.userUrl();
+            }
+            $location.url(url);
             return;
         }
 
         $scope.Session = Session;
         $scope.UrlFactory = UrlFactory;
         $scope.UserProfile= UserProfile;
+        $scope.path = path;
     }]);
