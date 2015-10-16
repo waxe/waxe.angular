adccd39c35f753d2631c2f5f7c17d3774319fa04
Aurélien Matouillot <aurelien@gymglish.com>
2015-10-16 13:06:39 +0200

    Shortcut for the search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,13 +10,18 @@
 angular.module('waxeApp')
     .controller('MainCtrl', ['$scope', '$location', 'UrlFactory', 'UserProfile', 'Session', function ($scope, $location, UrlFactory, UserProfile, Session) {
 
+        // Support path and search shortcut
         var path = $location.search().path;
+        var search = $location.search().search;
 
         if (UserProfile.has_file === true) {
             var url;
             if (angular.isDefined(path)) {
                 // TODO: remove this hardcoded path when we will be able to edit more filetype
                 url = UrlFactory.userUrl('xml/edit', {path: path});
+            }
+            else if (angular.isDefined(search)){
+                url = UrlFactory.userUrl('search', {search: search});
             }
             else {
                 url = UrlFactory.userUrl();
@@ -30,4 +35,5 @@ angular.module('waxeApp')
         $scope.UrlFactory = UrlFactory;
         $scope.UserProfile= UserProfile;
         $scope.path = path;
+        $scope.search = search;
     }]);
