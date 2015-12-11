0a822f4c51eae7861c2ebcd8ae14c88b8496e547
Aurélien Matouillot <a.matouillot@gmail.com>
2015-12-11 21:11:28 +0100

    Create a File and Folder factory

--- app/scripts/services/files.js
+++ app/scripts/services/files.js
@@ -7,10 +7,17 @@
  * # Files
  * Service in the waxeApp.
  */
-angular.module('waxeApp')
-    .service('Files', ['$http', 'UrlFactory', 'MessageService', 'Session', function ($http, UrlFactory, MessageService, Session) {
 
-        var File = function (data) {
+
+
+
+angular.module('waxeApp')
+    .factory('FS', function() {
+        var FS = function(data) {
+            this.init(data);
+        };
+
+        FS.prototype.init = function(data) {
             this.status = false;  // Versioning status
             this.selected = false;  // If it is selected by checkbox
 
@@ -20,27 +27,41 @@ angular.module('waxeApp')
                 }
             }
         };
+        FS.prototype._editUrl = function() {
+            throw new Error('NotImplemented');
+        };
 
-        Object.defineProperty(File.prototype, 'editUrl', {
+        Object.defineProperty(FS.prototype, 'editUrl', {
             get: function editUrlProperty() {
-                var url = '';
-                if (angular.isDefined(this.editor) && this.editor !== null) {
-                    url = this.editor + '/edit';
-                }
-                return UrlFactory.userUrl(url, {path: this.path});
+                return this._editUrl();
             }
         });
-
-        Object.defineProperty(File.prototype, 'iClass', {
-            get: function iClassProperty() {
-                if (this.type === 'folder') {
-                    return 'fa fa-folder-o';
-                }
-                else {
-                    return 'fa fa-file-excel-o';
-                }
-            }
-        });
+        return FS;
+    })
+    .factory('Folder', ['FS', 'UrlFactory', function(FS, UrlFactory) {
+        var Folder = function(data){
+            this.init(data);
+            this.iClass = 'fa fa-folder-o';
+        };
+        Folder.prototype = new FS();
+        Folder.prototype._editUrl = function() {
+            return UrlFactory.userUrl('', {path: this.path});
+        };
+        return Folder;
+    }])
+    .factory('File', ['FS', 'UrlFactory', function(FS, UrlFactory) {
+        var File = function(data){
+            this.init(data);
+            this.iClass = 'fa fa-file-excel-o';
+        };
+        File.prototype = new FS();
+        File.prototype._editUrl = function() {
+            var url = this.editor + '/edit';
+            return UrlFactory.userUrl(url, {path: this.path});
+        };
+        return File;
+    }])
+    .service('Files', ['$http', 'UrlFactory', 'MessageService', 'Session', 'Folder', 'File', function ($http, UrlFactory, MessageService, Session, Folder, File) {
 
         var getPaths = function(files) {
             var filenames = [];
@@ -54,7 +75,12 @@ angular.module('waxeApp')
             return $http[method](url, {params: {path: path}})
                 .then(function(res) {
                     return res.data.map(function(value) {
-                        return new File(value);
+                        if(value.type === 'folder') {
+                            return new Folder(value);
+                        }
+                        else {
+                            return new File(value);
+                        }
                     });
                 });
         };
