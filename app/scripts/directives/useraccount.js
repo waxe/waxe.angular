c766e52e35f6115c5e464cdf9edd02710f41d308
Aurélien Matouillot <a.matouillot@gmail.com>
2016-02-01 08:01:39 +0100

    Add link to open opened file in user account

--- app/scripts/directives/useraccount.js
+++ app/scripts/directives/useraccount.js
@@ -7,14 +7,19 @@
  * # UserAccount
  */
 angular.module('waxeApp')
-    .directive('useraccount', ['Session', 'UserProfile', function (Session, UserProfile) {
+    .directive('useraccount', ['$location', 'Session', 'UserProfile', 'File', function ($location, Session, UserProfile, File) {
         return {
-            template: '<div class="container alert-useraccount alert alert-warning" ng-if="Session.login != UserProfile.login">You are working on the account of <strong>{{Session.login}}</strong></div>',
+            template: '<div class="container alert-useraccount alert alert-warning" ng-if="Session.login != UserProfile.login">You are working on the account of <strong>{{Session.login}}</strong>. <a ng-if="Session.filename" href="" ng-click="openUserFile()">Open this file in your account</a></div>',
             restrict: 'E',
             replace: true,
             link: function postLink(scope) {
                 scope.Session = Session;
                 scope.UserProfile = UserProfile;
+                scope.openUserFile = function() {
+                    var file = File.loadFromPath(Session.filename);
+                    file.user = UserProfile.login;
+                    $location.url(file.editUrl);
+                };
             }
         };
     }]);
